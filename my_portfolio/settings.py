988e0f02dc3818bd13a0e56d541c9b8e32d16e4c
HARSH GARG <54450210+Harsh2607@users.noreply.github.com>
2025-01-24 01:10:41 +0530

    Update settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -67,7 +67,8 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': dj_database_url.config(conn_max_age=600)
+    # 'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
